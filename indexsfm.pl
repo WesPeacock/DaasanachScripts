352003de034ed1bb9799b158feeb324a99d3f421
Wes <wes_peacock@sil.org>
2021-09-07 10:26:44 -0600

    Fix the usage statement in indexsfm.pl

--- indexsfm.pl
+++ indexsfm.pl
@@ -1,4 +1,5 @@
-# sfm=mark perl -CSD -s -pf indexsfm.pl [sfm] <FileName.SFM
+# Usage:
+#   sfm=mark perl -CSD -pf indexsfm.pl <FileName.SFM
 # puts the line number on the end of all  lines with a marker specified on the command line.
 # default is \lx
 # only runs under bash (WSL or Git bash), not Windows CMD
