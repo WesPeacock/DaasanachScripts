d11c7bce082e0dac162346061f549bed7f1222e3
Wes <wes_peacock@sil.org>
2021-09-07 10:59:13 -0600

    Document how to specify multiplc SFMs in indexsfm.pl

--- indexsfm.pl
+++ indexsfm.pl
@@ -2,6 +2,8 @@
 #   sfm=mark perl -CSD -pf indexsfm.pl <FileName.SFM
 # puts the line number on the end of all  lines with a marker specified on the command line.
 # default is \lx
+# use single quotes/parentheses/bar construction to specify more than one sfm, E.g:
+# sfm='(lx|se|sse)' perl -CSD -pf indexsfm.pl <FileName.SFM
 # only runs under bash (WSL or Git bash), not Windows CMD
 BEGIN {
 	die "Run under git bash or WSL" if $^O !~ /(linux|msys)/;
